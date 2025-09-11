922ee153d2c185bb5c73249a132430fd4049b8a4
Hamza Elhakim <hamza.elhakim17@gmail.com>
2025-09-10 19:48:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -398,7 +398,7 @@ def generate_pdf():
         exam_time=datetime.utcnow().strftime("%H:%M:%S"),
         orig_base64=data["orig_base64"],
         proc_base64=data["proc_base64"],
-        model_name="Attention U-Net v1.0",
+        model_name=data["model_name"],
         processing_time=data.get("prediction_time", "N/A"),
         lesion_detected=data["lesion_detected"],
         avg_confidence=data["avg_confidence"],
